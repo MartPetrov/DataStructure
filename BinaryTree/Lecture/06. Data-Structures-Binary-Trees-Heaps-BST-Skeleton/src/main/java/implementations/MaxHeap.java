b5fcfd65148868d5cf365f40e9dd7b818656ad62
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2023-01-10 00:10:37 +0200

    MaxHeap

--- BinaryTree/Lecture/06. Data-Structures-Binary-Trees-Heaps-BST-Skeleton/src/main/java/implementations/MaxHeap.java
+++ BinaryTree/Lecture/06. Data-Structures-Binary-Trees-Heaps-BST-Skeleton/src/main/java/implementations/MaxHeap.java
@@ -8,18 +8,45 @@ import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
 
+    private List<E> elements;
+
+    public MaxHeap() {
+        this.elements = new ArrayList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
+    }
 
+    private void heapifyUp(int index) {
+        while (index > 0 && isLess(index,getParentIndex(index))) {
+            Collections.swap(this.elements, getParentIndex(index),index);
+            index = getParentIndex(index);
+        }
+    }
+
+    private boolean isLess(int parentIndex, int childIndex) {
+        return this.getAt(parentIndex).compareTo(getAt(childIndex)) > 0;
+    }
+
+    private E getAt(int index){ return this.elements.get(index);}
+
+    private int getParentIndex(int index) {
+        return (index - 1) / 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (this.size() == 0) {
+            throw new IllegalStateException("Illegal cal to peek on empty heap");
+        }
+        return this.getAt(0);
     }
 }
